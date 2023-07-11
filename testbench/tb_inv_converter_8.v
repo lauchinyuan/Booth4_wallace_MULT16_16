9e454a38f4ed15cc02cc8281154eaaa32ac42047
lauchinyuan <m13112458836@163.com>
2023-07-11 22:27:57 +0800

    Create mult_8_8 branch

--- testbench/tb_inv_converter_8.v
+++ testbench/tb_inv_converter_8.v
@@ -2,27 +2,27 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Author: lauchinyuan
 // Email: lauchinyuan@yeah.net
-// Create Date: 2023/04/29 22:21:18 
-// Module Name: tb_inv_converter_16
-// Description: testbench for inv_converter_16
+// Create Date: 2023/07/11 16:51:21
+// Module Name: tb_inv_converter_8
+// Description: testbench for inv_converter_8 module
 //////////////////////////////////////////////////////////////////////////////////
-module tb_inv_converter_16(
+module tb_inv_converter_8(
 
     );
-    reg [15:0]      data_i  ;
-    wire[16:0]      inv_o   ;
+    reg [7:0]      data_i  ;
+    wire[8:0]      inv_o   ;
     
     integer i;
     initial begin
-        data_i <= 16'h8000; //测试补码表示的下界是否能正常生成相反数
-        for(i=0;i<=65536;i=i+1) begin
+        data_i <= 8'h80; //测试补码表示的下界是否能正常生成相反数
+        for(i=0;i<=32768;i=i+1) begin
             #20
             data_i <= $random;
         end
     
     end
     
-    inv_converter_16 inv_converter_16_inst(
+    inv_converter_8 inv_converter_8_inst(
         .data_i (data_i ),//输入数据
 
         .inv_o  (inv_o  ) //输出相反数
--- testbench/tb_mult_8_8_top.v
+++ testbench/tb_mult_8_8_top.v
@@ -1,33 +1,35 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Author: lauchinyuan
-// Create Date: 2023/04/09 20:42:50
-// Module Name: tb_mult_16_16_top
-// Description: testbench for mult_16_16_top module
+// Author: lauchinyuan 
+// Email: lauchinyuan@yeah.net
+// Create Date: 2023/07/11 19:13:27
+// Module Name: tb_mult_8_8_top
+// Description: testbench for mult_8_8_top module
 //////////////////////////////////////////////////////////////////////////////////
-module tb_mult_16_16_top();
-    reg  signed [15:0]  A_NUM   ;
-    reg  signed [15:0]  B_NUM   ;
-    wire        [31:0]  C_NUM   ;
-    wire signed [31:0]  C_NUM_real;  //计算结果的真实值
+module tb_mult_8_8_top();
+
+    reg  signed [7:0]   A_NUM   ;
+    reg  signed [7:0]   B_NUM   ;
+    wire        [15:0]  C_NUM   ;
+    wire signed [15:0]  C_NUM_real;  //计算结果的真实值
     wire                correct ;    //计算结果正确标志
     
     //遍历所有可能的输入情况
-    //16bit有符号数的范围为[-32768,32767]
+    //8bit有符号数的范围为[-128,127]
     integer i,j;
     initial begin
         //初始值给定一个随机数
         A_NUM <= $random;
         B_NUM <= $random;
-        //遍历一轮数据需要操作进行(2^16 - 1)=65535次自增1运算
-        for(i=0;i<=65535;i=i+1) begin
-            for(j=0;j<=65535;j=j+1) begin
+        //遍历一轮数据需要操作进行(2^8 - 1)=255次自增1运算
+        for(i=0;i<=255;i=i+1) begin
+            for(j=0;j<=255;j=j+1) begin
                 //延时放在语句后面,使得A_NUM和B_NUM能同时变化
                 #20
-/*                 A_NUM <= i - 16'd32768; //A_NUM从-32768开始自增
-                B_NUM <= j - 16'd32768; //B_NUM从-32768开始自增 */
-                A_NUM <= $random;
-                B_NUM <= $random;             
+                A_NUM <= i - 8'd128; //A_NUM从-128开始自增
+                B_NUM <= j - 8'd128; //B_NUM从-128开始自增
+/*                 A_NUM <= $random;
+                B_NUM <= $random;     */         
             end
         end
     end
@@ -39,11 +41,11 @@ module tb_mult_16_16_top();
     assign correct = (C_NUM_real == C_NUM);
     
     //模块例化
-    mult_16_16_top mult_16_16_top_inst(
+    mult_8_8_top mult_8_8_top_inst(
         .A_NUM  (A_NUM),    //乘数
         .B_NUM  (B_NUM),    //被乘数
 
         .C_NUM  (C_NUM)     //积
     );
-    
+
 endmodule
